d0e683c247e3c08526cfa55530ada986e7d66752
arnobtarafder <rmtarnob@gmail.com>
2022-04-14 16:51:45 +0600

    checkbox system is connected with submit button fulfill

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
@@ -7,12 +7,13 @@ import SocialLogin from '../SocialLogin/SocialLogin';
 
 
 const Register = () => {
+    const [agree, setAgree] = useState(false);
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
         error,
-      ] = useCreateUserWithEmailAndPassword(auth);
+    ] = useCreateUserWithEmailAndPassword(auth);
 
     const navigate = useNavigate()
 
@@ -20,7 +21,7 @@ const Register = () => {
         navigate("/login");
     }
 
-    if(user){
+    if (user) {
         navigate("/home");
     }
 
@@ -29,6 +30,11 @@ const Register = () => {
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
+        const agree = event.target.terms.checked;
+
+        if (agree) {
+            createUserWithEmailAndPassword(email, password)
+        }
 
         createUserWithEmailAndPassword(email, password)
         // console.log(email);
@@ -36,19 +42,27 @@ const Register = () => {
 
     return (
         <div className='register-form mt-5'>
-            <h1 style={{textAlign: "center"}}>Please Register</h1>
+            <h1 style={{ textAlign: "center" }}>Please Register</h1>
 
             <form onSubmit={handleRegister}>
-                <input type="text" name='name' id=''placeholder='Your Name' />
+                <input type="text" name='name' id='' placeholder='Your Name' />
                 <br />
-                <input type="email" name='email' id='' placeholder='Email Address'/>
+                <input type="email" name='email' id='' placeholder='Email Address' />
                 <br />
                 <input type="password" name='password' id='' placeholder='Password' />
-                <input className='mt-3 me-2' type="checkbox" name="terms" id="terms" />
-                <label htmlFor="terms">Accept Genius Car Terms and Conditions</label>
-                <input className='btn btn-primary d-block mt-2' type="submit" value="Register" />
+
+                <input onClick={() => setAgree(!agree)} className='mt-3 me-2' type="checkbox" name="terms" id="terms" />
+                {/* <label className={agree ? 'text-primary' : 'text-danger'} htmlFor="terms">Accept Genius Car Terms and Conditions</label> */}
+                <label className={`ms-0 ${agree ? '' : 'text-danger'}`} htmlFor="terms">Accept Genius Car Terms and Conditions</label>
+
+                <input
+                    disabled = {!agree}
+                    className='btn btn-primary d-block mt-2'
+                    type="submit"
+                    value="Register" />
                 <br />
             </form>
+
             <p>Already have an account? <Link to="/login" className='text-danger text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
             <SocialLogin />
         </div>
