4d3057e512b5d396cb827d919a190e144b665f20
arnobtarafder <rmtarnob@gmail.com>
2022-04-15 20:32:59 +0600

    single service responsiveness by custom css

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import './ServiceDetail.css'
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
@@ -17,17 +18,21 @@ const ServiceDetail = () => {
 
     return (
         <div>
-            <h1 className='my-5 ms-5 border-start border-primary w-25 ps-4 py-2'>Product Id: {serviceId}</h1>
+            <h1 className='my-5 ms-5 border-start border-primary ps-4 py-2'>Product Id: {serviceId}</h1>
             
-          <div style={{margin:"0px auto"}} className='service rounded w-100 position-relative mt-5 px-auto ps-5 pt-5 text-center'>
-            <img className='w-50 border-start border-top border-primary p-5 md:w-100 sm:w-100' src={service[serviceId-1]?.img} alt="" />
-            <div className='p-3'>
-            <h2>{service[serviceId-1]?.name}</h2>
-            <h4 className='text-sans fw-normal pb-3'>Price: {service[serviceId-1]?.price}</h4>
+          <div style={{margin:"0px auto"}} className='service rounded w-100 px-auto pt-5 text-center'>
 
-            <p style={{textAlign: "justify"}} className='pb-5 w-50 lh-base fs-4 mx-auto'><small>{service[serviceId-1]?.description}</small></p>
+           <div id='image'>
+           <img className='border-start w-100 border-top border-primary p-5' src={service[serviceId-1]?.img} alt="" />
            </div>
-         </div>     
+            
+            <div className=''>
+                <h2>{service[serviceId-1]?.name}</h2>
+                <h4 className='text-sans fw-normal pb-3'>Price: {service[serviceId-1]?.price}</h4>
+
+                <p id='description' style={{textAlign: "justify"}} className='pb-5 w-50 lh-base fs-4 mx-auto'><small>{service[serviceId-1]?.description}</small></p>
+            </div>
+          </div>     
 
             <div className='text-center'>
 
