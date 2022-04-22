4c0aa57a1b86d0a8ea6fd6dde5e0a6e92668af88
arnobtarafder <rmtarnob@gmail.com>
2022-04-22 14:49:33 +0600

    validateDOMNesting <div> under <p> error solved

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -14,14 +14,14 @@ const Service = ({ service }) => {
             <div className='p-3'>
                 <h2>{name}</h2>
                 <p>Price: {price}</p>
-                <p className='pb-5 fs-5'><small>{
+                <div className='pb-5 fs-5'><small>{
                     description.length < 378
                         ? description
                         :
                         <div>
                             {description.slice(0, 278)}<span className=''><button onClick={() => navigateToServiceDetail(id)} className='btn btn-link text-decoration-none ps-0'>...read more</button></span> 
                         </div>
-                    }</small></p>
+                    }</small></div>
                 <button onClick={() => navigateToServiceDetail(id)} className='btn btn-primary position-absolute top-auto bottom-0'>Book Now</button>
             </div>
         </div>
