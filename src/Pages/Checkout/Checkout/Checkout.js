1385259aa154374dbc2f2d84f67067c4197de42d
arnobtarafder <rmtarnob@gmail.com>
2022-04-25 17:58:17 +0600

    input value implement by special therapy

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useServiceDetail from '../../../hooks/useServiceDetail';
 
 const Checkout = () => {
     const { serviceId } = useParams()
     const [service] = useServiceDetail(serviceId);
+    const [user, setUser] = useState({
+        name: "Roton Lal",
+        email: "lal@roton.com",
+        address: "Lalpur mohol para",
+        phone: "015695259",
+    })
+
+    const handleAddressChange = event => {
+        console.log(event.target.value);
+        const {address, ...rest} = user;
+        const newAddress = event.target.value;
+        const newUser = {address: newAddress, ...rest};
+        console.log(newUser);
+        setUser(newUser);
+    }
 
     return (
-        <div className='mt-5 ms-5'>
+        <div className='w-50 mx-auto'>
             <h2>Please Order: {service.name}</h2>
-            <h2>Please give me your money</h2>
-            <h2>Otherwise you will be dead</h2>
+            <form>
+                <input className='w-100 mb-2' type="text" name="name" value={user.name} required/><br />
+                <input className='w-100 mb-2' type="email" name="email" value={user.email} required/><br />
+                <input className='w-100 mb-2' type="text" name="service" value={service?.name} required/><br />
+                <input className='w-100 mb-2' type="text" name="address" onChange={handleAddressChange} value={user.address} required/><br />
+                <input className='w-100 mb-2' type="number" name="phone" value={user.phone} required/><br />
+
+                <input className='btn btn-primary' type="submit" value="Place Order" />
+            </form>
         </div>
     );
 };
