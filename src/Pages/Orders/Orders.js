351f4b8815ee54d18e22b7f274a3a6d5ce945961
arnobtarafder <rmtarnob@gmail.com>
2022-04-26 16:05:00 +0600

    axios private implemented successfully

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,37 +1,33 @@
-import axios from 'axios'
 import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
+import axiosPrivate from '../../api/axiosPrivate';
 import auth from '../../firebase.init';
 
 const Orders = () => {
     const [user] = useAuthState(auth);
     const [orders, setOrders] = useState([]);
     const navigate = useNavigate();
-    useEffect( () => {
+    useEffect(() => {
 
-        const getOrders = async() => {
+        const getOrders = async () => {
             const email = user.email
             const url = `http://localhost:520/order?email=${email}`;
-            try{
-                const {data} = await axios.get(url, {
-                    headers: {
-                        authorization: `Bearer ${localStorage.getItem("accessToken")}`
-                    }
-                })
+            try {
+                const { data } = await axiosPrivate.get(url)
                 setOrders(data);
             }
-            catch(error){
+            catch (error) {
                 console.log(error.message);
-                if(error.response.status === 403 || error.response.status === 401){
+                if (error.response.status === 403 || error.response.status === 401) {
                     signOut(auth)
                     navigate("/login")
                 }
             }
         }
         getOrders()
-    },[user])
+    }, [user])
 
     return (
         <div>
